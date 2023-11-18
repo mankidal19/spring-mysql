ff84c12c1caf7bef44278132f81de72601ae2940
kakmon <man.kidal19@gmail.com>
2023-11-19 06:44:01 +0800

    create user model

---
+++ src/main/java/com/example/spring/mysql/springmysql/model/User.java
@@ -0,0 +1,51 @@
+package com.example.spring.mysql.springmysql.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+/**
+ * User
+ */
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+}
